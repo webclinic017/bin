8f97dc274d34636a8b33496ff4c080a3be16c7a4
Shubham Priyadarshi <sss.priyadarshi608@gmail.com>
2021-08-29 02:01:34 +0530

    changes

--- xxx/connect/generate_access_token.py
+++ xxx/connect/generate_access_token.py
@@ -1,11 +1,30 @@
+import sys, getopt
 from kiteconnect import KiteTicker, KiteConnect
 
 local_data = open('../connect/base_path/local_data.txt', 'r').read()
 
-if __name__ == '__main__':
+def main(argv):
+    request_token = ''
+    try:
+        opts, args = getopt.getopt(argv,"h:r:",["request_token="])
+    except getopt.GetoptError:
+        print("exception occurred")
+        print('generate_access_token.py -r <request_token>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('generate_access_token.py -r <request_token>')
+            sys.exit()
+        elif opt in ("-r", "--request_token"):
+            request_token = arg
+
+    if request_token == '':
+        print("request token required..")
+        print('generate_access_token.py -rt <request_token>')
+        sys.exit(2)
+
     api_key = open(local_data + '/kite_api_key.txt', 'r').read()
     api_secret = open(local_data + '/kite_api_secret.txt', 'r').read()
-    request_token = open(local_data + '/kite_request_token.txt', 'r').read()
 
     kite = KiteConnect(api_key=api_key)
 
@@ -15,5 +34,8 @@ if __name__ == '__main__':
     access_token = kite_session['access_token']
 
     print(access_token)
-    with open('base_path/access_token.txt', 'w') as ak:
+    with open(local_data + '/kite_access_token.txt', 'w') as ak:
         ak.write(access_token)
+
+if __name__ == '__main__':
+   main(sys.argv[1:])
